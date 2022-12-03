b86b3659327c530c32e428ed7f17d282e1c070ac
M1sterJuniorRobot <s-8916@yandex.ru>
2022-12-03 13:37:36 +0300

    New files my first site.

---
+++ deepsvyat/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def newspapper(request):
+    return HttpResponse("<h4>Тут будут выводиться новости </h4>")
