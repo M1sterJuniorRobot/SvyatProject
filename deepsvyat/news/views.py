0ecc57160a4594d9b8604b2601e2f1fdd8891582
M1sterJuniorRobot <s-8916@yandex.ru>
2022-12-04 19:33:51 +0300

    Update 4.12.2022
    Last update for the day
    
    New form for create new-post
    
    New style in create post

--- deepsvyat/news/views.py
+++ deepsvyat/news/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 
 # Create your views here.
 def news_home(request):
-    news = Articles.objects.order_by('date')
+    news = Articles.objects.order_by('date')[:3]
     return render(request, 'news/news_home.html', {'news': news})
 
+
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
