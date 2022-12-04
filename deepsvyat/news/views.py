4ced950c05451a564bb35b96e8e41aedf1d270f2
M1sterJuniorRobot <s-8916@yandex.ru>
2022-12-04 16:56:35 +0300

    Update 4.12.2022 Add new page - create Refactor frontend code Add new form for add news Add button

--- deepsvyat/news/views.py
+++ deepsvyat/news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
 
 
 # Create your views here.
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.order_by('date')
+    return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    return render(request, 'news/create.html')
