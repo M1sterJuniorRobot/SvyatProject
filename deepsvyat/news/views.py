761d1562544f93a316f3eebc71a0f3a7e3797dad
M1sterJuniorRobot <s-8916@yandex.ru>
2022-12-06 16:23:26 +0300

    Update 6.12.2022
    Add new web-page for everyone elements in news
    
    Add button for go a new web-page with news

--- deepsvyat/news/views.py
+++ deepsvyat/news/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 # Create your views here.
 def news_home(request):
     news = Articles.objects.order_by('date')[:3]
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def create(request):
