ddffa68868ec1ee55af3906b319b76d66eb3bae3
M1sterJuniorRobot <s-8916@yandex.ru>
2023-05-26 14:19:54 +0300

    Update 26.05.2023 2:19 PM *added new styles in forms + fixed bug with profiles

--- deepsvyat/main/views.py
+++ deepsvyat/main/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'Hello', '123']
+        'title': 'Главная страница'
     }
     return render(request, 'main/index.html', data)
 
